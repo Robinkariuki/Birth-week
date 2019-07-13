93ed5d09965c9e573f4a6dbef060c8792096aa29
Robin <robinkaris7@gmail.com>
2019-07-13 19:21:10 +0300

    this commit will add three arrays for female,male and week

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 var DD = parseInt(prompt("Enter a Day: "));
 var MM  = parseInt(prompt("Enter a Month number: "));
 var YYinput = prompt("Enter a Year:");
-var gender
+var gender =  prompt("Enter Male or Female");
+alert(gender);
+
+var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+​
