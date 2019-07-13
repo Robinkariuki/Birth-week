6de8da485a5043fdf2439b7b7dc085d94910f724
Robin <robinkaris7@gmail.com>
2019-07-13 19:36:11 +0300

    this commit will add formula to js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-var DD = parseInt(prompt("Enter a Day: "));
-var MM  = parseInt(prompt("Enter a Month number: "));
-var YYinput = prompt("Enter a Year:");
-var gender =  prompt("Enter Male or Female");
+var century, year, month, date;
+
+document.getElementById('theform').onsubmit = function akanName() { console.log(document.getElementById('theform').value);
+var dob = document.getElementById('dob').value;
+alert('Your date of birth is ' + dob);
+var century = dob.slice(0,2);
+var year = dob.slice(2,4);
+var month = dob.slice(5,7);
+var date = dob.slice(8,10);
+
+var gender = prompt("Enter Male or Female");
 alert(gender);
 
-var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-​
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+
+var DD = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"];
+
+var dayOfBirth = parseInt(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10))) % 7;
+   if (gender === "Male") {
+       var mName = maleName[dayOfBirth - 1]
+       var day = DD[dayOfBirth -1]
+       alert(" Since you were born on " + day + " Your name is "+ Mname);
+       }
+       else if (gender === "Female") {
+           var Fname = femaleName[dayOfBirth -1]
+           var day = DD[dayOfBirth - 1]
+           alert(" Since you were born on " + day + " Your name is "+ Fname);
+       }
+
+}
+akanName();
