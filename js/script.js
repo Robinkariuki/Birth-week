ef2a6ba3975fe4d833eac844a669bf7ecb780085
Robin <robinkaris7@gmail.com>
2019-07-13 19:13:56 +0300

    this commit will declare variables

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-var DayMonth = parseInt(prompt("Enter Day of the month"));
-var Month =  parseInt(prompt("Enter  month"));
-var Year =  parseInt(prompt("Enter Year"));
-var cc = year.slice(0,2)
-
-function calculate(){
-return (((cc/4)-2*cc-1)+((5*year/4))+((26*(Month+1)/10))+DayMonth)%7;
-}
-
-  console.log(calculate);
+var DD = parseInt(prompt("Enter a Day: "));
+var MM  = parseInt(prompt("Enter a Month number: "));
+var YYinput = prompt("Enter a Year:");
+var gender
