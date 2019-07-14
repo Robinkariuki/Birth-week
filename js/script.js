a1947fa4c18d2ad4a8d184e1bf8a1284599090cf
Robin <robinkaris7@gmail.com>
2019-07-14 19:37:52 +0300

    this commit will add images directory

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ var year = dob.slice(2,4);
 var month = dob.slice(5,7);
 var date = dob.slice(8,10);
 
-var gender = prompt("Enter Male or Female");
-alert(gender);
+
 
 var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
@@ -19,14 +18,16 @@ var DD = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunda
 
 var dayOfBirth = parseInt(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10))) % 7;
    if (gender === "Male") {
-       var mName = maleName[dayOfBirth - 1]
-       var day = DD[dayOfBirth -1]
-       alert(" Since you were born on " + day + " Your name is "+ Mname);
+       var mName = maleName[dayOfBirth - 1];
+       alert("Your Akhan Name is "+ mName);
+       document.getElementById("displayName").innerHTML =  "Your Akhan Name is "+ mName;
+
+
        }
        else if (gender === "Female") {
            var Fname = femaleName[dayOfBirth -1]
            var day = DD[dayOfBirth - 1]
-           alert(" Since you were born on " + day + " Your name is "+ Fname);
+           document.getElementById("displayName").innerHTML = "Your Akhan Name is "+ fName;
        }
 
 }
